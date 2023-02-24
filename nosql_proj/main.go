d0d3162308f94b4d09a4d2e78173e7f127fdcb65
DigitalTroller <67204007+DigitalTroller@users.noreply.github.com>
2023-02-24 06:11:11 +0600

    Add files via upload

---
+++ nosql_proj/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/DigitalTroller/nosql_proj/configs"
+	"github.com/DigitalTroller/nosql_proj/middleware"
+	"github.com/DigitalTroller/nosql_proj/routes" //add this
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
+	"net/http"
+)
+
+func main() {
+	r := gin.New()
+
+	configs.ConnectDB()
+	r.Static("/css", "./css")
+
+	r.Use(gin.Logger())
+	routes.UserRoutes(r)
+
+	r.Use(middleware.Authentication())
+
+	// API-2
+	r.GET("/api-1", func(c *gin.Context) {
+		c.Redirect(http.StatusFound, "/task")
+	})
+
+	// API-1
+	r.GET("/api-2", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-2"})
+	})
+
+	r.Run(":" + "4000")
+
+}
